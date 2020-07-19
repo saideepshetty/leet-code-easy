24f3d620c4c4992b21504868fd6a1c517a3e1ebd
saideepshetty <66931645+saideepshetty@users.noreply.github.com>
2020-07-19 00:30:41 -0500

    Add files via upload

---
+++ symmetricTree.py
@@ -0,0 +1,38 @@
+'''
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ '''
+
+ # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root):
+        return Solution.isMirror(root,root)
+    
+    def isMirror(t1,t2):
+        if t1==None and t2==None:
+            return True
+        if t1==None or t2==None:
+            return False
+        return(t1.val==t2.val) and Solution.isMirror(t1.right,t2.left) and Solution.isMirror(t1.left,t2.right)
