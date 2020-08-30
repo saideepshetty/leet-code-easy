db0bb18913bc147c6a6703951d24ad6f750eb8b7
Saideep Niranjan Shetty <66931645+saideepshetty@users.noreply.github.com>
2020-08-30 13:59:22 -0500

    Add files via upload

---
+++ trailingZerosV1.py
@@ -0,0 +1,23 @@
+'''
+Given an integer n, return the number of trailing zeroes in n!.
+
+Example 1:
+
+Input: 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+Example 2:
+
+Input: 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+Note: Your solution should be in logarithmic time complexity.
+'''
+class Solution(object):
+    def trailingZeroes(self, n):
+        t = 0
+        power = 1
+        while n > 0:
+            n = n // 5**power
+            t += n
+        return t
