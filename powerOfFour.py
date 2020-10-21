9cf9c6b1b67a7d10bf90a2c1483ead0ed8175abd
Saideep Niranjan Shetty <66931645+saideepshetty@users.noreply.github.com>
2020-10-20 22:24:42 -0500

    Add files via upload

---
+++ powerOfFour.py
@@ -0,0 +1,27 @@
+'''
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 5
+Output: false
+Follow up: Could you solve it without loops/recursion?
+'''
+class Solution:
+    def isPowerOfFour(self, num: int) -> bool:
+        if num == 4 or num == 1: 
+            return True
+        elif num % 2 == 1:
+            return False
+        else:
+            while num > 4 :
+                den = num % 4
+                num = num / 4
+            if num == 4 and den == 0:
+                return True
+            else:
+                return False
