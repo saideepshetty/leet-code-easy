5b275895e991c056237b8265b072c68ed729a093
Saideep Niranjan Shetty <66931645+saideepshetty@users.noreply.github.com>
2020-11-07 15:10:45 -0600

    Add Digits

--- addDigits.py
+++ addDigits.py
@@ -9,6 +9,7 @@ Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2.
              Since 2 has only one digit, return it.
 Follow up:
 Could you do it without any loop/recursion in O(1) runtime?
+Code By Saideep Shetty
 '''
 class Solution:
     def addDigits(self, num: int) -> int:
