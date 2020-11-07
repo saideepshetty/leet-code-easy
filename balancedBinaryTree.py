da04d0a1fc8697bd41fbb7a39f38ff9927871522
Saideep Niranjan Shetty <66931645+saideepshetty@users.noreply.github.com>
2020-11-07 15:11:22 -0600

    Balanced Binary Tree

--- balancedBinaryTree.py
+++ balancedBinaryTree.py
@@ -4,6 +4,7 @@ Given a binary tree, determine if it is height-balanced.
 For this problem, a height-balanced binary tree is defined as:
 
 a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+Code By Saideep Shetty
 '''
 # Definition for a binary tree node.
 # class TreeNode:
