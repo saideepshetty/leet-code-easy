71e72df30d9b5e283c9ffab01a8ea8ea30b80b0b
Saideep Niranjan Shetty <66931645+saideepshetty@users.noreply.github.com>
2020-11-25 23:19:06 -0600

    First appearance of needle in haystack

--- implementStrStr.py
+++ implementStrStr.py
@@ -16,6 +16,7 @@ Clarification:
 What should we return when needle is an empty string? This is a great question to ask during an interview.
 
 For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+Code By Saideep Shetty
 '''
 
 class Solution(object):
