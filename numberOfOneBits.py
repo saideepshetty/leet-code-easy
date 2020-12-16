4e354a69475c97970f97058898a178e588ba58df
Saideep Niranjan Shetty <66931645+saideepshetty@users.noreply.github.com>
2020-12-15 23:13:04 -0600

    Number of One bits

--- numberOfOneBits.py
+++ numberOfOneBits.py
@@ -24,7 +24,7 @@ Note:
 
 Note that in some languages such as Java, there is no unsigned integer type. In this case, the input will be given as signed integer type and should not affect your implementation, as the internal binary representation of the integer is the same whether it is signed or unsigned.
 In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3 above the input represents the signed integer -3.
-
+Code By Saideep Shetty
 '''
 
 class Solution(object):
