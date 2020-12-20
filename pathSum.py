4bacd10aabf909838323cdc7641a282b9441e2bc
Saideep Niranjan Shetty <66931645+saideepshetty@users.noreply.github.com>
2020-12-19 23:21:08 -0600

    Path Sum (Binary Tree)

--- pathSum.py
+++ pathSum.py
@@ -15,6 +15,7 @@ Given the below binary tree and sum = 22,
  /  \      \
 7    2      1
 return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+Code By Saideep Shetty
 '''
 
 # Definition for a binary tree node.
