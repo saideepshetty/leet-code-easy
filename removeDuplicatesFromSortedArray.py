fa8745355e45f221ef86f63542fcd4e32189a384
Saideep Niranjan Shetty <66931645+saideepshetty@users.noreply.github.com>
2021-02-02 23:05:31 -0600

    Remove Duplicates from Sorted Array

--- removeDuplicatesFromSortedArray.py
+++ removeDuplicatesFromSortedArray.py
@@ -22,6 +22,7 @@ Clarification:
 Confused why the returned value is an integer but your answer is an array?
 
 Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+Code By Saideep Shetty
 '''
 
 class Solution:
